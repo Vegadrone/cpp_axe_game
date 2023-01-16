9eecb1494697ec543fb4afedbe9c68e1a6e09e36
Vegadrone <spenaantonino@gmail.com>
2023-01-16 17:21:08 +0100

    feat: :sparkles: add new varables
    add diffrent type variables

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,6 +7,16 @@ int main(){
     
     int w; 
     w = 900;
+
+    float root_beer;
+    root_beer = 1.99;
     
+    double cheese_burger;
+    cheese_burger = 5.99;
+
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
+
     InitWindow(h, w, "POPUP");
 }
