206c0e14c3b00e1f2add8159bc8c83d08dc4d124
Vegadrone <spenaantonino@gmail.com>
2023-01-17 15:22:05 +0100

    refactor: :zap: change code to make it more readable

--- axe_game.cpp
+++ axe_game.cpp
@@ -9,15 +9,19 @@ int main(){
     */
 
     //Windows dimensions
-    int h = 750;
-    int w = 900;
+    int w = 800;
+    int h = 450;
 
-    InitWindow(h, w, "POPUP");
+    InitWindow(w, h, "POPUP");
 
     //Circle coordinates
-    int circle_x {750/2};
-    int circle_y {900/2};
+    int circle_y {w/2};
+    int circle_x {h/2};
     int circle_radius {50};
+
+    //Axe Coordinates
+    int axe_x {400};
+    int axe_y {0};
 
     SetTargetFPS(60);
     while (WindowShouldClose() == false)
@@ -28,25 +32,29 @@ int main(){
         //Game logic begins
 
         DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, 150, 150, WHITE);
 
-        if (IsKeyDown(KEY_D) && circle_x < (750 - circle_radius))
+        //axe movement
+        axe_y += 10;
+
+        if (IsKeyDown(KEY_D) && circle_x < (w - circle_radius))
         {
-            circle_x = circle_x + 10;
+            circle_x += 10;
         }
 
         if (IsKeyDown(KEY_A) && circle_x > (0 + circle_radius))
         {
-            circle_x = circle_x - 10;
+            circle_x -= 10;
         }
 
         if (IsKeyDown(KEY_W) && circle_y > (0 + circle_radius))
         {
-            circle_y = circle_y - 10;
+            circle_y -= 10;
         }
 
-        if (IsKeyDown(KEY_S) && circle_y < (900 - circle_radius) )
+        if (IsKeyDown(KEY_S) && circle_y < (h - circle_radius) )
         {
-            circle_y = circle_y + 10;
+            circle_y += 10;
         }
 
         //Game logic ends
