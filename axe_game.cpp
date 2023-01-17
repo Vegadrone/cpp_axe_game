7bf238e7c60a993f30b6ab427825a58ef8771641
Vegadrone <spenaantonino@gmail.com>
2023-01-17 16:09:22 +0100

    feat: :sparkles: Add game over text
    Game over text appear when collision is set to true

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,7 +1,5 @@
 #include "raylib.h"
 
-
-
 int main(){
     
     /*
@@ -27,7 +25,7 @@ int main(){
     //Axe Coordinates
     int axe_x {400};
     int axe_y {0};
-    int axe_direction {10};
+    int axe_direction {25};
     int axe_length {50};
     //Axe edges
     int l_axe_x{axe_x};
@@ -35,48 +33,58 @@ int main(){
     int u_axe_y{axe_y};
     int b_axe_y{axe_y + axe_length};
 
+    //Axe player collision
+    bool collision_with_axe {true};
+
     SetTargetFPS(60);
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(RED);
 
-        //Game logic begins
-
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_length, axe_length, WHITE);
-
-        //axe movement
-        axe_y += axe_direction;
-
-        if (axe_y > (h - axe_length) || axe_y < 0)
+        if (collision_with_axe)
         {
-            axe_direction = -axe_direction;
+            DrawText("GAME OVER!!!", 235, 200, 50, YELLOW);
         }
         
-
-        //Circle movement
-        if (IsKeyDown(KEY_D) && circle_x < (w - circle_radius))
+        else
         {
-            circle_x += 10;
-        }
+            //Game logic begins
 
-        if (IsKeyDown(KEY_A) && circle_x > (0 + circle_radius))
-        {
-            circle_x -= 10;
-        }
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, WHITE);
 
-        if (IsKeyDown(KEY_W) && circle_y > (0 + circle_radius))
-        {
-            circle_y -= 10;
-        }
+            //axe movement
+            axe_y += axe_direction;
 
-        if (IsKeyDown(KEY_S) && circle_y < (h - circle_radius) )
-        {
-            circle_y += 10;
-        }
+            if (axe_y > (h - axe_length) || axe_y < 0)
+            {
+                axe_direction = -axe_direction;
+            }
+            
 
-        //Game logic ends
-        EndDrawing();
+            //Circle movement
+            if (IsKeyDown(KEY_D) && circle_x < (w - circle_radius))
+            {
+                circle_x += 10;
+            }
+
+            if (IsKeyDown(KEY_A) && circle_x > (0 + circle_radius))
+            {
+                circle_x -= 10;
+            }
+
+            if (IsKeyDown(KEY_W) && circle_y > (0 + circle_radius))
+            {
+                circle_y -= 10;
+            }
+
+            if (IsKeyDown(KEY_S) && circle_y < (h - circle_radius) )
+            {
+                circle_y += 10;
+            }
+            //Game logic ends
+        }
+            EndDrawing();
     }
 }
